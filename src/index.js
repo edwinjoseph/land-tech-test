82ec2b2a432e06b82c328b441d1a1769397156d4
Edwin Joseph <edwin@wonderbly.com>
2021-03-09 18:17:44 +0000

    feat(route/company): create a new route to display company info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const db = require('./db');
+const company = require('./controllers/company');
 const app = express();
 const port = 3000;
 
 app.get('/', (_, res) => {
   res.json({ ok: true });
 });
+
+app.get('/ownership/company/:id', async (req, res) => {
+  try {
+    const data = await company.getCompany(req.params.id, db);
+    res.json({ data });
+  } catch (err) {
+    res.status(404).json({ data: null });
+  }
+})
 
 app.listen(port, () => {
   console.info(`Express server running on port ${port}`)
